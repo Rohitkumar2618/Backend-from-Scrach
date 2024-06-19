f84a800e422f1a753c673562441f23262999c15c
Rohitkumar2618 <rohitkumarmankar26.akrk@gmail.com>
2024-06-19 23:01:23 +0530

    mastering fn

---
+++ mastering_Function.js
@@ -0,0 +1,28 @@
+
+// 1 function statement
+
+function abcd{
+
+}
+
+// 2 Anonymous function
+
+function(){
+
+}
+
+//  3. fat arrow fn
+
+()=>{
+
+}
+
+// 4. fat arrow function with one argument
+
+a => {
+
+}
+
+//  5. arrow function with direct return value
+
+() => 12
