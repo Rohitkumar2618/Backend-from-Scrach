159818a7ece1c18406d3712c63126fd0b082217c
Rohitkumar2618 <rohitkumarmankar26.akrk@gmail.com>
2024-06-27 22:29:37 +0530

    basic Routing

--- Node/HTTP/index.js
+++ Node/HTTP/index.js
@@ -1,7 +1,17 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("Hello, world!");
+  //! req ---> Matlb jo frontend se aata hai
+
+  if (req.url === "/") {
+    res.end("Home Page");
+  }
+  if (req.url === "/profile") {
+    res.end("Profile Page");
+  } else {
+    // res.writeHead(404);
+    res.end("Page not found");
+  }
 });
 
 server.listen(3000);
